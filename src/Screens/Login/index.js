c38ef9cffb274165a314ec99ed1fe67a3efbd8b1
Benedict <BThomas.QBT@emaar.ae>
2020-06-26 21:04:07 +0530

    added api call

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import styles from './styles';
-const Login = () => {
+import {connect} from 'react-redux';
+const Login = ({logIn}) => {
+  const onButtonPressed = () => {
+    let params = [];
+    // props.logIn vilikunathinu pakaram logIn ne deconstruct cheythit call cheyunu
+    logIn(params);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.textStyle}>Login Page</Text>
+      <Button onPress={() => onButtonPressed()} />
     </View>
   );
 };
 
-export default Login;
+const mapDispatchToProps = ({authModel: {logIn}}) => ({logIn});
+
+export default connect(null, mapDispatchToProps)(Login);
